edb31a82d92eff3a9cf630084f8d4326cc414e69
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-03-05 12:56:44 +0300

    main updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,41 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // Person sınıfından nesneler oluşturuluyor
+        Person person1 = new Person("John", "Doe", 20);
+        Person person2 = new Person("Jane", "Smith", 17);
+        Person person3 = new Person("Alice", "Johnson", 14, "1234 Elm St", "555-1234", "alice@example.com");
+
+        // Person bilgilerini yazdırma
+        System.out.println("Person 1:");
+        System.out.println("Firstname: " + person1.getFirstName());
+        System.out.println("LastName: " + person1.getLastName());
+        System.out.println("Age: " + person1.getAge());
+        System.out.println("Is Teen: " + person1.isTeen());
+
+        System.out.println("\nPerson 2:");
+        System.out.println("Firstname: " + person2.getFirstName());
+        System.out.println("LastName: " + person2.getLastName());
+        System.out.println("Age: " + person2.getAge());
+        System.out.println("Is Teen: " + person2.isTeen());
+
+        System.out.println("\nPerson 3:");
+        System.out.println("Firstname: " + person3.getFirstName());
+        System.out.println("LastName: " + person3.getLastName());
+        System.out.println("Age: " + person3.getAge());
+        System.out.println("Is Teen: " + person3.isTeen());
+        System.out.println("Address: " + person3.address);
+        System.out.println("Phone: " + person3.phoneNumber);
+        System.out.println("Email: " + person3.email);
+
+        // Wall sınıfından nesne oluşturuluyor
+        Wall wall = new Wall(5, 4);
+        System.out.println("\nArea of the Wall: " + wall.getArea());
+
+        // Set height to negative value and print new values
+        wall.setHeight(-1.5);
+        System.out.println("Width: " + wall.getWidth());
+        System.out.println("Height: " + wall.getHeight());
+        System.out.println("Area: " + wall.getArea());
     }
 }
